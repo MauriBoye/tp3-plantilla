26c327609ac6c69113ceb0aa3c5ea17a9a0de69f
Mauricio Boyé <78159999+MauriBoye@users.noreply.github.com>
2021-05-13 19:19:19 -0300

    Sexto punto T.P N°3
    Imprima todas las líneas que contengan una dirección de correo electrónica (solo considerar palabras que contengan un @)

--- boye_mauricio_grep1.sh
+++ boye_mauricio_grep1.sh
@@ -4,3 +4,4 @@ grep -E "^[[:digit:]]{3}[ ]" grepdata.txt
 grep -E "^[[:alpha:]]{3}[.][ ][[:digit:]]{1,2}[,][ ][2][[:digit:]]{3}" grepdata.txt
 grep -E -i "[aeiou][[:alpha:]][aeiou]" grepdata.txt
 grep -E "^[^S]" grepdata.txt
+grep -E "@" grepdata.txt
